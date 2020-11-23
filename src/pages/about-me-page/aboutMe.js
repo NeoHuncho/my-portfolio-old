cd0dab4deab266f12f1f8859ebb67689b3199770
NeoHuncho <william.guinaudie@gmail.com>
2020-11-23 17:40:26 +0100

    bug fixing

--- src/pages/about-me-page/aboutMe.js
+++ src/pages/about-me-page/aboutMe.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import FrontPage from './front-page/frontPage';
-import CodingEducation from './coding-education/CodingEducation';
+import CodingEducation from './coding-education/codingEducation';
 import WhatIKnow from './what-i-know/whatIKnow'
 export default function aboutMe() {
     return (
