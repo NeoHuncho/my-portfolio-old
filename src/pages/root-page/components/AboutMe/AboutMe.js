fe5fdc66814e8ffda009b930034adca0363158b5
NeoHuncho <william.guinaudie@gmail.com>
2020-12-16 11:13:48 +0100

    added material ui button

--- src/pages/root-page/components/AboutMe/AboutMe.js
+++ src/pages/root-page/components/AboutMe/AboutMe.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { motion } from "framer-motion";
 import styled from 'styled-components';
 
-import {Page, Title,TitleSection,LineBottom,LineTop} from '../../../../styles'
+import { Page, Title, TitleSection, LineBottom, LineTop } from '../../../../styles'
 import '../general.css';
+//import {AboutMe as aboutme}  from '../../../about-me-page/aboutMe';
 
-import Button from 'react-bootstrap/Button';
+import Button from '@material-ui/core/Button';
 import { useInView } from 'react-intersection-observer';
-import astraunaut from './files/astraunaut.svg';
 import { Link } from 'react-router-dom';
 
+import astraunaut from './files/astraunaut.svg';
 const Svg = styled(motion.img)`
 
 position:relative;
@@ -32,30 +33,30 @@ function AboutMe() {
   })
   return (
     < Page style={{ zIndex: 2 }}>
-     
-    <TitleSection ref={ref} initial={{ opacity: 0, x: -50 }}  animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -50 }} transition={{ duration: 1, ease: "easeOut" }}>
-    <Link to='/about-me' >
-      <Title > About<br />Me</Title>
-    </Link>
-          <LineTop />
-          <LineBottom />
-          <Link to='/about-me'>  <Button  className='button' size="lg" variant="info">Show Me More</Button></Link>
+
+      <TitleSection ref={ref} initial={{ opacity: 0, x: -50 }} animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -50 }} transition={{ duration: 1, ease: "easeOut" }}>
+        <Link to='/about-me' >
+          <Title > About<br />Me</Title>
+        </Link>
+        <LineTop />
+        <LineBottom />
+        <Button variant="contained" color="primary" component={aboutme}> Show Me More!</Button>
       </TitleSection>
 
-     <motion.div initial={{ opacity: 0 }} animate={{ opacity: inView ? 1 : 0 }}style={{ zIndex: 0 }}>
-        <Link to='/about-me'> 
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: inView ? 1 : 0 }} style={{ zIndex: 0 }}>
+        <Link to='/about-me'>
           <Svg
-          animate={{ y: [0, -10, 0] }} transition={{
-            duration: 5,
-            ease: "easeInOut",
-            loop: Infinity
-          }}  src={astraunaut} alt='Computer' />
+            animate={{ y: [0, -10, 0] }} transition={{
+              duration: 5,
+              ease: "easeInOut",
+              loop: Infinity
+            }} src={astraunaut} alt='Computer' />
         </Link>
       </motion.div>
 
-     
-      
-      
+
+
+
     </Page>
   )
 }
