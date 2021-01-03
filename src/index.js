9efcfeb18dc594869e2cc8b0e69d9da4ecced9b6
NeoHuncho <william.guinaudie@gmail.com>
2021-01-03 12:50:57 +0100

    fixed routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import App from './App';
+import ScrollToTop from './ScrolllToTop';
+import FrontPage from './pages/root-page/components/FrontPage/FrontPage';
+import MyProjectsRoot from './pages/root-page/components/MyProjects/MyProjects';
+import AboutMeRoot from './pages/root-page/components/AboutMe/AboutMe';
+import NavBar from './pages/NavBar/NavBar'
+import AboutMe from './pages/about-me-page/aboutMe';
+import MyProjects from './pages/my-projects-page/myProjects';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function App() {
+  return (
+ 
+    <>
+      <ScrollToTop />
+      <div>
+        <NavBar />
+        
+        <Route exact path="/" render={e => (
+          <>
+            <FrontPage />
+            <MyProjectsRoot id='projects' />
+            <AboutMeRoot id='about-me' />
+          </>
+        )} />
+          <Route path="/about-me" component={AboutMe} />
+          <Route path="/my-projects" component={MyProjects} />
+      </div>
+    </>
+
+  );
+}
+const rootElement= document.getElementById('root');
+ReactDOM.render(<Router onUpdate={() => window.scrollTo(0, 0)} ><App /></Router>, rootElement)
 
