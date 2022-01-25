66dd131fa3531553625818a3d95712a4d5c96ad5
Neo Huncho <william.guinaudie@gmail.com>
2022-01-25 17:54:42 +0100

    added info to my project

--- src/pages/my-projects-page/projects/tabs/full-stack-projects/FullStackProjects.js
+++ src/pages/my-projects-page/projects/tabs/full-stack-projects/FullStackProjects.js
@@ -3,34 +3,35 @@ import { motion } from "framer-motion";
 import {
   ProjectCard,
   TechnologiesSection,
-  GLink,
-  Github,
+  // GLink,
+  // Github,
   ProjectPage,
   Technology,
   TitleSection,
   GLplatform,
   Firebase,
-  Express,
+  // Express,
   NodeJS,
 } from "../card_styles";
 
 import react from "../../../specialties/Tabs/FrontEndTechnologies/LogoSource/react.png";
 import python from "../../../specialties/Tabs/ProgramingLanguages/logos/python.png";
 import redux from "../../../specialties/Tabs/FrontEndTechnologies/LogoSource/redux.png";
+import styledComponents from "../../../specialties/Tabs/FrontEndTechnologies/LogoSource/styledcomponents.png";
 import fireBase from "../../../specialties/Tabs/DatabaseTechnologies/LogoSource/firebase.png";
-import mongodb from "../../../specialties/Tabs/DatabaseTechnologies/LogoSource/mongodb.png";
-import express from "../../../specialties/Tabs/BackEndTechnologies/LogoSource/express.png";
+// import mongodb from "../../../specialties/Tabs/DatabaseTechnologies/LogoSource/mongodb.png";
+// import express from "../../../specialties/Tabs/BackEndTechnologies/LogoSource/express.png";
 import glPlatform from "../../../specialties/Tabs/BackEndTechnologies/LogoSource/gcPlatform.png";
 import nodejs from "../../../specialties/Tabs/BackEndTechnologies/LogoSource/nodejs.png";
-import github from "../../../specialties/Tabs/StorageDeployment/LogoSource/github.png";
-import heroku from "../../../specialties/Tabs/StorageDeployment/LogoSource/heroku.png";
+// import github from "../../../specialties/Tabs/StorageDeployment/LogoSource/github.png";
+// import heroku from "../../../specialties/Tabs/StorageDeployment/LogoSource/heroku.png";
 
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
-import memories from "./images/memories.webp";
+import greenCrypto from "./images/greenCrypto.png";
 import brawlMax from "./images/brawlMax.png";
 
 function FrontEndProjects() {
@@ -76,41 +77,41 @@ function FrontEndProjects() {
 
         <ProjectCard>
           <CardActionArea
-            href="https://memories-mernstack.netlify.app/"
+            href="https://www.greencrypto.wiki/"
             target="_blank"
             style={{ textDecoration: "none" }}
           >
             <CardMedia
               component="img"
-              alt="Memories Project"
+              alt="Green Crypto Project"
               height="180"
-              image={memories}
-              title="Memories Project"
+              image={greenCrypto}
+              title="Green Crypto Project"
             />
             <CardContent>
               <TitleSection>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Memories{" "}
+                  Green Crypto{" "}
                 </Typography>
-                <GLink
+                {/* <GLink
                   href="https://github.com/NeoHuncho/memories-MERN"
                   target="_blank"
                 >
                   <Github src={github} />
-                </GLink>
+                </GLink> */}
               </TitleSection>
 
               <Typography variant="body2" color="textSecondary" component="p">
-                MERN project/ Create posts w/image, like and delete
-                functionality.
+                Website listing various crypto currencies and there impact on
+                the environment.
               </Typography>
               <TechnologiesSection>
                 <Technology title="React" src={react} />
                 <Technology title="Redux" src={redux} />
-                <Technology title="MongoDb" src={mongodb} />
-                <Express title="Express" src={express} />
+                <Technology title="Styled Components" src={styledComponents} />
                 <NodeJS title="NodeJs" src={nodejs} />
-                <Technology title="Heroku" src={heroku} />
+                <GLplatform title="Google Cloud Platform" src={glPlatform} />
+                <Firebase title="Firebase" src={fireBase} />
               </TechnologiesSection>
             </CardContent>
           </CardActionArea>
